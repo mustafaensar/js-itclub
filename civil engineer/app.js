618f69867f9de8db0691a276cf360743be7419b6
mustafaensar <mustafaensar55@gmail.com>
2020-01-29 18:13:38 +0100

    new homework

---
+++ civil engineer/app.js
@@ -0,0 +1,23 @@
+let DaireAlani = DaireAlan(5);
+
+console.log ("Daire Alani", DaireAlani); 
+
+let DikdortgenAlani = DortgenAlan(8, 5);
+
+console.log ("Dikdortgen Alani", DikdortgenAlani)
+
+let DaireCevresi = DaireCevre(4);
+
+console.log ("Daire Cevresi", DaireCevresi)
+
+let DikdortgenCevresi = DortgenCevre(5, 6);
+
+console.log ("Dikdortgen Cevresi", DikdortgenCevresi)
+
+let KupunHacmi = KupHacim(4);
+
+console.log ("Kupun Hacmi", KupunHacmi)
+
+let KupunAlani = KupAlan(8);
+
+console.log ("Kupun Alani", KupunAlani)
---
+++ civil engineer/hesaplama_metodlari.js
@@ -0,0 +1,29 @@
+function DaireAlan(r) {
+  let sonuc = Math.round(Math.PI) * Math.pow(r, 2);
+  return sonuc;
+}
+
+function DortgenAlan(a, b) {
+  let sonuc = (a * b);
+  return sonuc;
+}
+
+function DaireCevre(r) {
+  let sonuc = (2 * (Math.round(Math.PI)) * r);
+  return sonuc;
+}
+
+function DortgenCevre(a, b) {
+  let sonuc = (2 * (a + b))
+  return sonuc;
+}
+
+function KupHacim(x) {
+  let sonuc = Math.pow(x, 3);
+  return sonuc;
+}
+
+function KupAlan(x) {
+  let sonuc = (x * 6);
+  return sonuc;
+}
